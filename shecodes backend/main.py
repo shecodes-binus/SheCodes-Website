d9d8391ca08db283fce3e461cf91ba844c7f61e0
Tyrone Vargas <vargasthevargas@gmail.com>
2025-05-20 18:10:22 +0700

    import backend

---
+++ shecodes backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from .routers import documentation, user, event, mentor, partner, alumni, faq, contact, blog
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(documentation.router)
+app.include_router(user.router)
+app.include_router(event.router)
+app.include_router(mentor.router)
+app.include_router(partner.router)
+app.include_router(alumni.router)
+app.include_router(faq.router)
+app.include_router(contact.router)
+app.include_router(blog.router)
+
+# uhh might add on to this later
