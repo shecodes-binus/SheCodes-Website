91342594f40967ed164affa0bc8eb17bc5f8673d
Tyrone Vargas <vargasthevargas@gmail.com>
2025-06-09 00:59:45 +0700

    update event model

--- shecodes backend/models/event.py
+++ shecodes backend/models/event.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column, Integer, String, Enum, DateTime, Text
+from sqlalchemy import Column, Integer, String, Enum, DateTime, Text, Table, ForeignKey
+from sqlalchemy.orm import relationship
 from ..database import Base
 from datetime import datetime
+
+event_mentor_association = Table(
+    "event_mentor_association",
+    Base.metadata,
+    Column("event_id", Integer, ForeignKey("events.id")),
+    Column("mentor_id", Integer, ForeignKey("mentors.id"))
+)
 
 class Event(Base):
     __tablename__ = "events"
@@ -12,4 +20,39 @@ class Event(Base):
     start_date = Column(DateTime, nullable=False)
     end_date = Column(DateTime, nullable=False)
     location = Column(String, nullable=False)
+    tools = Column(String, nullable=False)
+    key_points = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+    mentors = relationship("Mentor", secondary=event_mentor_association, backref="events")
+    skills = relationship("Skill", cascade="all, delete-orphan", backref="event")
+    benefits = relationship("Benefit", cascade="all, delete-orphan", backref="event")
+    sessions = relationship("Session", cascade="all, delete-orphan", backref="event")
+
+class Skill(Base):
+    __tablename__ = "skills"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(Text, nullable=False)
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
+
+
+class Benefit(Base):
+    __tablename__ = "benefits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    text = Column(Text, nullable=False)
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    topic = Column(String, nullable=False)
+    description = Column(Text, nullable=False)
+    start = Column(DateTime, nullable=False)
+    end = Column(DateTime, nullable=False)
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
