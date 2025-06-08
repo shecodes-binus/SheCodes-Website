37dc45bd0a4164bc0448c242a356beef6998ec25
Tyrone Vargas <vargasthevargas@gmail.com>
2025-06-09 01:02:59 +0700

    update event schema

--- shecodes backend/schemas/event.py
+++ shecodes backend/schemas/event.py
@@ -26,3 +26,41 @@ class EventResponse(EventBase):
 
     class Config:
         orm_mode = True
+
+class SkillBase(BaseModel):
+    title: str
+    description: str
+
+class SkillCreate(SkillBase):
+    pass
+
+class SkillResponse(SkillBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class BenefitBase(BaseModel):
+    title: str
+    text: str
+
+class BenefitCreate(BenefitBase):
+    pass
+
+class BenefitResponse(BenefitBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class SessionBase(BaseModel):
+    topic: str
+    description: str
+    start: datetime
+    end: datetime
+
+class SessionCreate(SessionBase):
+    pass
+
+class SessionResponse(SessionBase):
+    id: int
+    class Config:
+        orm_mode = True
